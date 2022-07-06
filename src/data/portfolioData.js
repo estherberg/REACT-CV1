4b752e10b3abbd8256b3fabfb1cb9015bdc02c46
esther bergman <estherbergman16@gmail.com>
2022-07-07 00:50:10 +0200

    ca va

---
+++ src/data/portfolioData.js
@@ -0,0 +1,38 @@
+export const portfolioData = [
+  {
+    id: 1,
+    name: 'Moodle',
+    languages: ['php','javascript','css'],
+    languagesIcons: ['fab fa-js','fab fa-php','fab fa-css3-alt'],
+    source: 'https://github.com/estherberg/moodle',
+    info: "The application must allow a student to connect and access according to his grade to courses, documents and teachers. The teacher can create a course and manage student documents and grades. The application is developed as a server-based rich client application.",
+    picture: './media/project1.JPG'
+  },
+  {
+    id: 2,
+    name: 'Be Biker',
+    languages: ['html','css'],
+    languagesIcons: ['fab fa-html5','fab fa-css3-alt'],
+    source: 'https://github.com/estherberg/bebiker',
+    info: "This website makes it possible to present the Be Biker company, its activity, its universe and its passion for cycling, as well as to offer an overview of the repair, advice and sale of equipment in real time.",
+    picture: './media/project2.JPG'
+  },
+  {
+    id: 3,
+    name: 'GAME ESCAPE',
+    languages: ['html','css'],
+    languagesIcons: ['fab fa-html5','fab fa-css3-alt'],
+    source: 'https://github.com/estherberg/gamescape',
+    info: "This website presents the escape game organised by my university. He's composed of three pages: home, scenarios and reservation.",
+    picture: './media/project3.JPG'
+  },
+  {
+    id: 5,
+    name: 'Connect 4',
+    languages: ['C'],
+    languagesIcons: ['fa-regular fa-c'],
+    source: 'https://github.com/estherberg/connect4',
+    info: 'It’s the classic game you love to play with your friends and family. Can you connect four of your disks by dropping them into the holder before your opponent does?',
+    picture: './media/project5.JPG'
+  }
+]
